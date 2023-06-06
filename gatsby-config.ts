c8a88183de60040a53ecd738575847a4a7b9e16f
GimpFather <filip.galczyk13@gmail.com>
2023-06-06 16:02:16 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,21 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Patryk-Jopek-Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "BYpefNe_04_AUIIfOhOr1FSACMm_WMiJaPxzFlBTtXM",
+      "spaceId": "75513wvkur1c"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+};
+
+export default config;
